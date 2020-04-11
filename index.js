3b2b4d40a710b81fbe8b95c058bff767ebb2a3d8
naman03malhotra <naman03malhotra@gmail.com>
2020-04-12 02:14:19 +0530

    bfs, iterative+recursive

--- index.js
+++ index.js
@@ -1,3 +1,93 @@
+const graph = {
+  a: {
+    c: 1,
+    d: 1,
+  },
+  b: {
+    c: 1,
+  },
+  c: {
+    a: 1,
+    b: 1,
+    d: 1,
+    e: 1,
+  },
+  d: {
+    a: 1,
+    c: 1,
+    f: 1,
+  },
+  e: {
+    c: 1,
+  },
+  f: {
+    d: 1,
+  },
+};
 
 
-console.log(12);
+function bfs(graph) {
+  let covered = {}, queue = [], order = [], toExplore;
+
+  for (v in graph) {
+    if(covered[v]) {
+      continue;
+    }
+    covered[v] = 1;
+    queue.push(v);
+
+    while(queue.length != 0) {
+      toExplore = queue.shift()
+      order.push(toExplore);
+
+      for(e in graph[toExplore]) {
+        if(covered[e]) {
+          continue;
+        }
+        covered[e] = 1;
+        queue.push(e);
+      }
+    }
+  }
+
+  return order;
+}
+
+
+function recursiveUtil(graph, queue, covered, order) {
+  if(!queue.length) {
+    return;
+  }
+  let toExplore = queue.shift();
+
+  for(e in graph[toExplore]) {
+    if(covered[e]) {
+      continue;
+    }
+    covered[e] = 1;
+    queue.push(e);
+    order.push(e);
+  }
+
+  recursiveUtil(graph, queue, covered, order);
+}
+
+function recursiveBfs(graph) {
+  let covered = {}, queue = [], order = [], toExplore;
+
+  for (v in graph) {
+    if(covered[v]) {
+      continue;
+    }
+    covered[v] = 1;
+    queue.push(v);
+    order.push(v);
+
+    recursiveUtil(graph, queue, covered, order);
+  }
+
+  return order;
+}
+
+console.log(bfs(graph));
+console.log(recursiveBfs(graph));
